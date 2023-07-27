66c65c44b51d0335842be24ebac8dbde39853684
David <davetech112@gmail.com>
2023-07-27 16:30:32 +0100

    first commit

---
+++ src/main/java/com/example/SecurityWorkTrhough/SecurityWorkTrhoughApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecurityWorkTrhough;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWorkTrhoughApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityWorkTrhoughApplication.class, args);
+	}
+
+}
